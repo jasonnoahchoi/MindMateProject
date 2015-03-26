267d2644e5615fefd6e029e6f9cd951ecbc15e46
Jason Choi <jasonnoahchoi@gmail.com>
2015-03-26 16:53:45 -0600

    Initial Commit

---
+++ MindMate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MindMate
+//
+//  Created by Jason Noah Choi on 3/26/15.
+//  Copyright (c) 2015 Jason Choi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MindMate/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MindMate
+//
+//  Created by Jason Noah Choi on 3/26/15.
+//  Copyright (c) 2015 Jason Choi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
