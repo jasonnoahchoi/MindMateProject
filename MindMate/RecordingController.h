5f30142f71af5ef049c4fa05c8669f789347f298
Jason Choi <jasonnoahchoi@gmail.com>
2015-03-26 18:30:37 -0600

    Set up controller

--- MindMate/RecordingController.h
+++ MindMate/RecordingController.h
@@ -7,7 +7,22 @@
 //
 
 #import <Foundation/Foundation.h>
+@class Recording, Group, User;
 
 @interface RecordingController : NSObject
+
+@property (nonatomic, strong, readonly) NSArray *memos;
+@property (nonatomic, strong, readonly) NSArray *memoNames;
+
++ (RecordingController *)sharedInstance;
+
+- (void)save;
+- (void)addRecordingWithName:(NSString *)memoName;
+- (void)addRecordingWithFile:(NSData *)memo;
+- (void)addGroupWithName:(NSString *)groupName;
+- (void)removeRecording:(Recording *)recording;
+- (void)removeGroup:(Group *)group;
+- (void)addRecordingToGroup:(Group *)group;
+- (void)receiveRecordingFromUser:(User *)user;
 
 @end
--- MindMate/RecordingController.m
+++ MindMate/RecordingController.m
@@ -7,7 +7,92 @@
 //
 
 #import "RecordingController.h"
+#import "Stack.h"
+#import "Recording.h"
+#import "Group.h"
+#import "User.h"
+
+@interface RecordingController ()
+
+@property (nonatomic, strong) NSArray *memos;
+
+@end
+
+static NSString * const recordingEntity = @"Recording";
+static NSString * const userEntity = @"User";
+static NSString * const groupEntity = @"Group";
 
 @implementation RecordingController
+
++ (RecordingController *)sharedInstance {
+    static RecordingController *sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[RecordingController alloc] init];
+    });
+    return sharedInstance;
+}
+
+- (NSArray *)memos {
+    NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:recordingEntity];
+    return [[Stack sharedInstance].managedObjectContext executeFetchRequest:fetchRequest error:NULL];
+}
+
+- (NSArray *)memoNames {
+    NSMutableArray *mutableMemoNames = [[NSMutableArray alloc] init];
+    for (Recording *recording in self.memos) {
+        NSString *memoName = recording.memoName;
+        [mutableMemoNames addObject:memoName];
+        NSData *memoFile = recording.memo;
+        [mutableMemoNames addObject:memoFile];
+        User *memoFromUser = recording.fromUser;
+        [mutableMemoNames addObject:memoFromUser];
+    }
+    return mutableMemoNames;
+}
+
+- (void)save {
+    [[Stack sharedInstance].managedObjectContext save:NULL];
+}
+- (void)addRecordingWithName:(NSString *)memoName {
+    Recording *recording = [NSEntityDescription insertNewObjectForEntityForName:recordingEntity inManagedObjectContext:[Stack sharedInstance].managedObjectContext];
+    recording.memoName = memoName;
+    [self save];
+}
+- (void)addRecordingWithFile:(NSData *)memo {
+    Recording *recording = [NSEntityDescription insertNewObjectForEntityForName:recordingEntity inManagedObjectContext:[Stack sharedInstance].managedObjectContext];
+    recording.memo = memo;
+    [self save];
+
+}
+- (void)addGroupWithName:(NSString *)groupName {
+    Group *group = [NSEntityDescription insertNewObjectForEntityForName:groupEntity inManagedObjectContext:[Stack sharedInstance].managedObjectContext];
+    group.groupName = groupName;
+    [self save];
+
+}
+
+- (void)removeRecording:(Recording *)recording {
+    [recording.managedObjectContext deleteObject:recording];
+    [self save];
+}
+
+- (void)removeGroup:(Group *)group {
+    [group.managedObjectContext deleteObject:group];
+    [self save];
+
+}
+
+- (void)addRecordingToGroup:(Group *)group {
+    Recording *recording = [NSEntityDescription insertNewObjectForEntityForName:recordingEntity inManagedObjectContext:[Stack sharedInstance].managedObjectContext];
+    [recording setGroup:group];
+    [self save];
+}
+
+- (void)receiveRecordingFromUser:(User *)user {
+    Recording *recording = [NSEntityDescription insertNewObjectForEntityForName:recordingEntity inManagedObjectContext:[Stack sharedInstance].managedObjectContext];
+    [recording setFromUser:user];
+    [self save];
+}
 
 @end
