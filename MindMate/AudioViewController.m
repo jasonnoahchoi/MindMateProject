fbcffb2a148ed21afde98560946967ff2c3c236b
Jason Choi <jasonnoahchoi@gmail.com>
2015-04-05 03:40:13 -0600

    Refactor code to make it cleaner and make containerView buttons pop when you click on them

--- MindMate/AudioViewController.m
+++ MindMate/AudioViewController.m
@@ -23,8 +23,7 @@
 @property (nonatomic, strong) UIButton *recordAgainButton;
 @property (nonatomic, strong) UILabel *confirmLabel;
 @property (nonatomic, strong) UILabel *recordAgainLabel;
-@property (nonatomic, strong) UIButton *invisibleConfirmButton;
-@property (nonatomic, strong) UIButton *invisibleRecordAgainButton;
+
 @property (nonatomic, assign) CGPoint centerRecordAgainButton;
 @property (nonatomic, assign) CGPoint centerConfirmButton;
 @property (nonatomic, assign) CGPoint endPointRecordAgainButton;
@@ -33,11 +32,11 @@
 @property (nonatomic, strong) UIButton *recordCornerButton;
 @property (nonatomic, strong) UIButton *playCornerButton;
 
-
 @property (nonatomic, assign) CGPoint centerRecordButton;
 @property (nonatomic, assign) CGPoint centerPlayButton;
 @property (nonatomic, assign) CGPoint endPointRecordCornerButton;
 @property (nonatomic, assign) CGPoint endPointPlayCornerButton;
+
 @property (nonatomic, assign) NSNumber *groupIDNumber;
 
 @end
@@ -118,12 +117,16 @@
     [self.view addSubview:self.containerView];
 }
 
-- (void)cornerButtons {
+- (void)layoutCornerEndPoints {
     CGRect endPointRecordCornerButton = CGRectMake(0 - self.view.frame.size.width/6, self.view.frame.size.height - self.view.frame.size.height/6, self.view.frame.size.width/2, self.view.frame.size.width/2);
     self.endPointRecordCornerButton = CGPointMake(endPointRecordCornerButton.origin.x + (endPointRecordCornerButton.size.width/2), endPointRecordCornerButton.origin.y + (endPointRecordCornerButton.size.height/2));
 
     CGRect endPointPlayCornerButton = CGRectMake(self.view.frame.size.width - self.view.frame.size.width/3, self.view.frame.size.height - self.view.frame.size.height/6, self.view.frame.size.width/2, self.view.frame.size.width/2);
     self.endPointPlayCornerButton = CGPointMake(endPointPlayCornerButton.origin.x + (endPointPlayCornerButton.size.width/2), endPointPlayCornerButton.origin.y + (endPointPlayCornerButton.size.height/2));
+}
+
+- (void)cornerButtons {
+    [self layoutCornerEndPoints];
 
     self.recordCornerButton = [[UIButton alloc] initWithFrame:CGRectMake(0 - self.view.frame.size.width/3, self.view.frame.size.height + self.view.frame.size.height/6, self.view.frame.size.width/2, self.view.frame.size.width/2)];
     [self.view addSubview:self.recordCornerButton];
@@ -143,10 +146,10 @@
     self.playCornerButton.layer.shouldRasterize = YES;
     self.centerPlayButton = self.playCornerButton.center;
     [self.playCornerButton addTarget:self action:@selector(cornerButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
-    [self addAnimation];
+    [self addPlayButtonAnimation];
 }
 
-- (void)addAnimation {
+- (void)addPlayButtonAnimation {
     [UIView animateWithDuration:.25 delay:0 options:UIViewAnimationOptionCurveEaseIn animations:^{
         self.playCornerButton.hidden = NO;
         self.playCornerButton.alpha = 1;
@@ -305,11 +308,7 @@
                         [UIView animateWithDuration:.2 delay:0 options:UIViewAnimationOptionCurveEaseIn animations:^{
                             button.transform = CGAffineTransformIdentity;
                             button.backgroundColor = [UIColor customPurpleColor];
-                        } completion:^(BOOL finished) {
-
-
-
-                        }];
+                        } completion:nil];
                     }];
                 }];
                 }
@@ -416,62 +415,6 @@
     [[AudioController sharedInstance] stopRecording];
 }
 
-
-//- (void)recordTap:(UIButton *)sender {
-//    if (sender.selected) {
-//        // stop
-//        [self.recorder stop];
-//        self.buttonView.playButton.enabled = YES;
-//        //[self.recordingTimer invalidate];
-//       // self.recordingTimer = nil;
-//        NSLog(@"%@", self.recorder.url);
-//        // [self.microphone stopFetchingAudio];
-//
-//        NSData *data = [NSData dataWithContentsOfURL:self.recorder.url];
-//        [data writeToFile:[NSHomeDirectory() stringByAppendingString:[NSString stringWithFormat:@"%@", [[AudioController sharedInstance] filePath]]] atomically:YES];
-//        //[data writeToFile:[NSHomeDirectory() stringByAppendingPathComponent:[NSString stringWithFormat:@"%@", [self filePath]]] atomically:YES];
-//        NSLog(@"Data File: %@", data);
-//        // [[RecordingController sharedInstance] addRecordingWithFile:data];
-//        [[RecordingController sharedInstance] addRecordingWithURL:[[AudioController sharedInstance] filePath]
-//                                                      andIDNumber:[[AudioController sharedInstance] randomIDNumber]
-//                                                   andDateCreated:[[AudioController sharedInstance] createdAtDate]
-//                                                     andFetchDate:[[AudioController sharedInstance] fetchDate]
-//                                                    andSimpleDate:[[AudioController sharedInstance] simpleDateString]
-//                                                     andGroupName:[[AudioController sharedInstance] groupName]];
-//        //[RecordingController sharedInstance] addGroupWithName:
-//
-//        NSLog(@"ControllerRecordingPath: %@", [[AudioController sharedInstance] filePath]);
-//        [[RecordingController sharedInstance] save];
-//        NSLog(@"Date Created: %@, Fetch Date: %@, IDNUMBER: %@", [[AudioController sharedInstance] createdAtDate], [[AudioController sharedInstance] fetchDate], [[AudioController sharedInstance] randomIDNumber]);
-//
-//        //[[NSFileManager defaultManager] createFileAtPath:[self filePath] contents:data attributes:nil];
-//
-//    } else {
-//        // start
-//        self.buttonView.playButton.enabled = NO;
-//
-//        self.recorder = [[AudioController sharedInstance] recordAudioToDirectory];
-//
-//        self.recorder.delegate = self;
-////        self.recordingTimer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(recordingTimerUpdate:) userInfo:nil repeats:YES];
-////        [self.recordingTimer fire];
-//        // [self.microphone startFetchingAudio];
-//
-//        // [self initializeViewController];
-//        //  [self drawBufferPlot];
-//
-//    }
-//    sender.selected = !sender.selected;
-//}
-
-//- (void)playTap:(id)sender
-//{
-//    //    [SimpleAudioPlayer playFile:_recorder.url.description];
-//    self.player.delegate = self;
-//
-//    NSLog(@"duration: %f", self.player.duration);
-//}
-
 - (void)hideBottomButtons {
     self.confirmLabel.hidden = YES;
     [UIView animateWithDuration:.4 delay:0 options:UIViewAnimationOptionCurveEaseIn animations:^{
@@ -529,7 +472,7 @@
         self.buttonView.recordButton.layer.backgroundColor = [UIColor customPurpleColor].CGColor;
         self.playCornerButton.hidden = NO;
         self.playCornerButton.alpha = 1;
-        [self addAnimation];
+        [self addPlayButtonAnimation];
     }];
 }
 
@@ -538,8 +481,13 @@
     [self showBottomButtons];
     self.groupIDNumber = @1;
     [UIView animateWithDuration:.3 animations:^{
+        self.containerView.focusButton.transform = CGAffineTransformScale(CGAffineTransformIdentity, 1.1, 1.1);
         self.buttonView.recordButton.layer.backgroundColor = [UIColor customGreenColor].CGColor;
         [self setAlphaOfBottomButtons];
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:.1 delay:0 options:UIViewAnimationOptionCurveEaseIn animations:^{
+            self.containerView.focusButton.transform = CGAffineTransformIdentity;
+        } completion:nil];
     }];
 }
 
@@ -548,9 +496,15 @@
     [self showBottomButtons];
     self.groupIDNumber = @2;
     [UIView animateWithDuration:.3 animations:^{
+        self.containerView.courageButton.transform = CGAffineTransformScale(CGAffineTransformIdentity, 1.1, 1.1);
         self.buttonView.recordButton.layer.backgroundColor = [UIColor redColor].CGColor;
         [self setAlphaOfBottomButtons];
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:.1 delay:0 options:UIViewAnimationOptionCurveEaseIn animations:^{
+            self.containerView.courageButton.transform = CGAffineTransformIdentity;
+        } completion:nil];
     }];
+
 }
 
 - (void)ambitionState:(ButtonState)state {
@@ -558,35 +512,58 @@
     [self showBottomButtons];
     self.groupIDNumber = @3;
     [UIView animateWithDuration:.3 animations:^{
+        self.containerView.ambitionButton.transform = CGAffineTransformScale(CGAffineTransformIdentity, 1.1, 1.1);
         self.buttonView.recordButton.layer.backgroundColor = [UIColor orangeColor].CGColor;
         [self setAlphaOfBottomButtons];
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:.1 delay:0 options:UIViewAnimationOptionCurveEaseIn animations:^{
+            self.containerView.ambitionButton.transform = CGAffineTransformIdentity;
+        } completion:nil];
     }];
+
 }
 - (void)imaginationState:(ButtonState)state {
     state = ButtonStateImagination;
     [self showBottomButtons];
     self.groupIDNumber = @4;
     [UIView animateWithDuration:.3 animations:^{
+        self.containerView.imaginationButton.transform = CGAffineTransformScale(CGAffineTransformIdentity, 1.1, 1.1);
         self.buttonView.recordButton.layer.backgroundColor = [UIColor purpleColor].CGColor;
         [self setAlphaOfBottomButtons];
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:.1 delay:0 options:UIViewAnimationOptionCurveEaseIn animations:^{
+            self.containerView.imaginationButton.transform = CGAffineTransformIdentity;
+        } completion:nil];
     }];
+
 }
 - (void)funState:(ButtonState)state {
     state = ButtonStateFun;
     [self showBottomButtons];
     self.groupIDNumber = @5;
     [UIView animateWithDuration:.3 animations:^{
+        self.containerView.funButton.transform = CGAffineTransformScale(CGAffineTransformIdentity, 1.1, 1.1);
         self.buttonView.recordButton.layer.backgroundColor = [UIColor cyanColor].CGColor;
         [self setAlphaOfBottomButtons];
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:.1 delay:0 options:UIViewAnimationOptionCurveEaseIn animations:^{
+            self.containerView.funButton.transform = CGAffineTransformIdentity;
+        } completion:nil];
     }];
 }
+
 - (void)presenceState:(ButtonState)state {
     state = ButtonStatePresence;
     [self showBottomButtons];
     self.groupIDNumber = @6;
     [UIView animateWithDuration:.3 animations:^{
+        self.containerView.presenceButton.transform = CGAffineTransformScale(CGAffineTransformIdentity, 1.1, 1.1);
         self.buttonView.recordButton.layer.backgroundColor = [UIColor yellowColor].CGColor;
         [self setAlphaOfBottomButtons];
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:.1 delay:0 options:UIViewAnimationOptionCurveEaseIn animations:^{
+            self.containerView.presenceButton.transform = CGAffineTransformIdentity;
+        } completion:nil];
     }];
 }
 
