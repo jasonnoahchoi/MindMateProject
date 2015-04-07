342b2e56c6d5a9f83f81885749b3c4a4d9e91820
Jason Choi <jasonnoahchoi@gmail.com>
2015-04-06 23:54:55 -0600

    Change colors of buttons to gray

--- MindMate/CategoryContainerView.m
+++ MindMate/CategoryContainerView.m
@@ -71,75 +71,75 @@
                 break;
         }
         
-        self.focusButton.layer.borderColor = [UIColor whiteColor].CGColor;
-        [self.focusButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-        self.courageButton.layer.borderColor = [UIColor whiteColor].CGColor;
-        [self.courageButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-        self.ambitionButton.layer.borderColor = [UIColor whiteColor].CGColor;
-        [self.ambitionButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-        self.imaginationButton.layer.borderColor = [UIColor whiteColor].CGColor;
-        [self.imaginationButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-        self.funButton.layer.borderColor = [UIColor whiteColor].CGColor;
-        [self.funButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-        self.presenceButton.layer.borderColor = [UIColor whiteColor].CGColor;
-        [self.presenceButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        self.focusButton.layer.borderColor = [UIColor darkGrayColor].CGColor;
+        [self.focusButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
+        self.courageButton.layer.borderColor = [UIColor darkGrayColor].CGColor;
+        [self.courageButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
+        self.ambitionButton.layer.borderColor = [UIColor darkGrayColor].CGColor;
+        [self.ambitionButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
+        self.imaginationButton.layer.borderColor = [UIColor darkGrayColor].CGColor;
+        [self.imaginationButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
+        self.funButton.layer.borderColor = [UIColor darkGrayColor].CGColor;
+        [self.funButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
+        self.presenceButton.layer.borderColor = [UIColor darkGrayColor].CGColor;
+        [self.presenceButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
 
 //        ButtonView *view = [[ButtonView alloc] init];
 //        [view gestureRecognizer:view.longPressGesture shouldRequireFailureOfGestureRecognizer:view.longPressGesture];
     }
     if (state == ButtonStateFocus) {
-        self.focusButton.layer.borderColor = [UIColor whiteColor].CGColor;
+        self.focusButton.layer.borderColor = [UIColor darkGrayColor].CGColor;
         self.focusButton.backgroundColor = [UIColor greenColor];
         [self.delegate focusState:ButtonStateFocus];
-        [self.focusButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [self.focusButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
     } else {
-        self.focusButton.layer.borderColor = [UIColor whiteColor].CGColor;
+        self.focusButton.layer.borderColor = [UIColor darkGrayColor].CGColor;
         self.focusButton.backgroundColor = [UIColor clearColor];
-        [self.focusButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [self.focusButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
     }
     if (state == ButtonStateCourage) {
-        self.courageButton.layer.borderColor = [UIColor whiteColor].CGColor;
+        self.courageButton.layer.borderColor = [UIColor darkGrayColor].CGColor;
         self.courageButton.backgroundColor = [UIColor redColor];
         [self.delegate courageState:ButtonStateCourage];
-        [self.courageButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [self.courageButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
     } else {
-        self.courageButton.layer.borderColor = [UIColor whiteColor].CGColor;
+        self.courageButton.layer.borderColor = [UIColor darkGrayColor].CGColor;
         self.courageButton.backgroundColor = [UIColor clearColor];
-        [self.courageButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [self.courageButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
     }
     if (state == ButtonStateAmbition) {
-        self.ambitionButton.layer.borderColor = [UIColor whiteColor].CGColor;
+        self.ambitionButton.layer.borderColor = [UIColor darkGrayColor].CGColor;
         self.ambitionButton.backgroundColor = [UIColor orangeColor];
         [self.delegate ambitionState:ButtonStateAmbition];
-        [self.ambitionButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [self.ambitionButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
     } else {
-        self.ambitionButton.layer.borderColor = [UIColor whiteColor].CGColor;
+        self.ambitionButton.layer.borderColor = [UIColor darkGrayColor].CGColor;
         self.ambitionButton.backgroundColor = [UIColor clearColor];
-        [self.ambitionButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [self.ambitionButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
     }
     if (state == ButtonStateImagination) {
         self.imaginationButton.layer.borderColor = [UIColor purpleColor].CGColor;
         [self.delegate imaginationState:ButtonStateImagination];
         [self.imaginationButton setTitleColor:[UIColor purpleColor] forState:UIControlStateNormal];
     } else {
-        self.imaginationButton.layer.borderColor = [UIColor whiteColor].CGColor;
-        [self.imaginationButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        self.imaginationButton.layer.borderColor = [UIColor darkGrayColor].CGColor;
+        [self.imaginationButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
     }
     if (state == ButtonStateFun) {
         self.funButton.layer.borderColor = [UIColor cyanColor].CGColor;
         [self.delegate funState:ButtonStateFun];
         [self.funButton setTitleColor:[UIColor cyanColor] forState:UIControlStateNormal];
     } else {
-        self.funButton.layer.borderColor = [UIColor whiteColor].CGColor;
-        [self.funButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        self.funButton.layer.borderColor = [UIColor darkGrayColor].CGColor;
+        [self.funButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
     }
     if (state == ButtonStatePresence) {
         self.presenceButton.layer.borderColor = [UIColor yellowColor].CGColor;
         [self.delegate presenceState:ButtonStatePresence];
         [self.presenceButton setTitleColor:[UIColor yellowColor] forState:UIControlStateNormal];
     } else {
-        self.presenceButton.layer.borderColor = [UIColor whiteColor].CGColor;
-        [self.presenceButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        self.presenceButton.layer.borderColor = [UIColor darkGrayColor].CGColor;
+        [self.presenceButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
     }
 }
 
@@ -175,42 +175,42 @@
     [self.focusButton setTitle:@"Focus" forState:UIControlStateNormal];
     self.focusButton.titleLabel.text = @"Focus";
     [self.focusButton addTarget:self action:@selector(focusPressed:) forControlEvents:UIControlEventTouchUpInside];
-    self.focusButton.layer.borderColor = [UIColor whiteColor].CGColor;
+    self.focusButton.layer.borderColor = [UIColor darkGrayColor].CGColor;
     self.focusButton.layer.borderWidth = 2;
     self.focusButton.layer.cornerRadius = 6;
 
     self.courageButton = [[UIButton alloc] init];
     [self.courageButton setTitle:@"Courage" forState:UIControlStateNormal];
     [self.courageButton addTarget:self action:@selector(couragePressed:) forControlEvents:UIControlEventTouchUpInside];
-    self.courageButton.layer.borderColor = [UIColor whiteColor].CGColor;
+    self.courageButton.layer.borderColor = [UIColor darkGrayColor].CGColor;
     self.courageButton.layer.borderWidth = 2;
     self.courageButton.layer.cornerRadius = 6;
 
     self.ambitionButton = [[UIButton alloc] init];
     [self.ambitionButton setTitle:@"Ambition" forState:UIControlStateNormal];
     [self.ambitionButton addTarget:self action:@selector(ambitionPressed:) forControlEvents:UIControlEventTouchUpInside];
-    self.ambitionButton.layer.borderColor = [UIColor whiteColor].CGColor;
+    self.ambitionButton.layer.borderColor = [UIColor darkGrayColor].CGColor;
     self.ambitionButton.layer.borderWidth = 2;
     self.ambitionButton.layer.cornerRadius = 6;
 
     self.imaginationButton = [[UIButton alloc] init];
     [self.imaginationButton setTitle:@"Imagination" forState:UIControlStateNormal];
     [self.imaginationButton addTarget:self action:@selector(imaginationPressed:) forControlEvents:UIControlEventTouchUpInside];
-    self.imaginationButton.layer.borderColor = [UIColor whiteColor].CGColor;
+    self.imaginationButton.layer.borderColor = [UIColor darkGrayColor].CGColor;
     self.imaginationButton.layer.borderWidth = 2;
     self.imaginationButton.layer.cornerRadius = 6;
 
     self.funButton = [[UIButton alloc] init];
     [self.funButton setTitle:@"Fun" forState:UIControlStateNormal];
     [self.funButton addTarget:self action:@selector(funPressed:) forControlEvents:UIControlEventTouchUpInside];
-    self.funButton.layer.borderColor = [UIColor whiteColor].CGColor;
+    self.funButton.layer.borderColor = [UIColor darkGrayColor].CGColor;
     self.funButton.layer.borderWidth = 2;
     self.funButton.layer.cornerRadius = 6;
 
     self.presenceButton = [[UIButton alloc] init];
     [self.presenceButton setTitle:@"Presence" forState:UIControlStateNormal];
     [self.presenceButton addTarget:self action:@selector(presencePressed:) forControlEvents:UIControlEventTouchUpInside];
-    self.presenceButton.layer.borderColor = [UIColor whiteColor].CGColor;
+    self.presenceButton.layer.borderColor = [UIColor darkGrayColor].CGColor;
     self.presenceButton.layer.borderWidth = 2;
     self.presenceButton.layer.cornerRadius = 6;
 
@@ -232,7 +232,7 @@
 
     NSDictionary *buttonsDictionary = NSDictionaryOfVariableBindings(_focusButton, _courageButton, _ambitionButton, _imaginationButton, _funButton, _presenceButton);
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|-[_focusButton]-[_courageButton(==_focusButton)]-[_ambitionButton(==_focusButton)]-|" options:0 metrics:nil views:buttonsDictionary]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|-[_imaginationButton]-[_funButton(==_imaginationButton)]-[_presenceButton(_imaginationButton)]-|"  options:0 metrics:nil views:buttonsDictionary]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|-[_imaginationButton]-[_funButton(==_imaginationButton)]-[_presenceButton(==_imaginationButton)]-|"  options:0 metrics:nil views:buttonsDictionary]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-[_focusButton]-[_imaginationButton(==_focusButton)]-|" options:0 metrics:nil views:buttonsDictionary]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-[_courageButton]-[_funButton(==_courageButton)]-|" options:0 metrics:nil views:buttonsDictionary]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-[_ambitionButton]-[_presenceButton(==_ambitionButton)]-|" options:0 metrics:nil views:buttonsDictionary]];
