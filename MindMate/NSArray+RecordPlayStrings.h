f95b27eccc4dd964f2210cc0f7d2e51ded41a06a
Jason Choi <jasonnoahchoi@gmail.com>
2015-04-15 14:10:44 -0600

    Edit colors

---
+++ MindMate/NSArray+RecordPlayStrings.h
@@ -0,0 +1,13 @@
+//
+//  NSArray+RecordPlayStrings.h
+//  Tomorrow
+//
+//  Created by Jason Noah Choi on 4/15/15.
+//  Copyright (c) 2015 Jason Choi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (RecordPlayStrings)
+
+@end
---
+++ MindMate/NSArray+RecordPlayStrings.m
@@ -0,0 +1,13 @@
+//
+//  NSArray+RecordPlayStrings.m
+//  Tomorrow
+//
+//  Created by Jason Noah Choi on 4/15/15.
+//  Copyright (c) 2015 Jason Choi. All rights reserved.
+//
+
+#import "NSArray+RecordPlayStrings.h"
+
+@implementation NSArray (RecordPlayStrings)
+
+@end
