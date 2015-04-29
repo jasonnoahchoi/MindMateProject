99975ac75304f1249d90527246ec914f0cd115b8
Jason Choi <jasonnoahchoi@gmail.com>
2015-04-29 14:41:45 -0700

    Create new menu view subclass for DRY code

---
+++ MindMate/MenuView.m
@@ -0,0 +1,21 @@
+//
+//  MenuView.m
+//  Tomorrow
+//
+//  Created by Jason Noah Choi on 4/29/15.
+//  Copyright (c) 2015 Jason Choi. All rights reserved.
+//
+
+#import "MenuView.h"
+
+@implementation MenuView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
