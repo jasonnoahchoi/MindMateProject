cb4d44fbbab79cb3808919e707a14f78c223f9ed
Jason Choi <jasonnoahchoi@gmail.com>
2015-05-01 23:02:29 -0700

    Add new nsdate category, view with play/record icon

---
+++ MindMate/RecordPlayView.h
@@ -0,0 +1,13 @@
+//
+//  RecordPlayView.h
+//  Tomorrow
+//
+//  Created by Jason Noah Choi on 5/1/15.
+//  Copyright (c) 2015 Jason Choi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecordPlayView : UIView
+
+@end
---
+++ MindMate/RecordPlayView.m
@@ -0,0 +1,21 @@
+//
+//  RecordPlayView.m
+//  Tomorrow
+//
+//  Created by Jason Noah Choi on 5/1/15.
+//  Copyright (c) 2015 Jason Choi. All rights reserved.
+//
+
+#import "RecordPlayView.h"
+
+@implementation RecordPlayView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
